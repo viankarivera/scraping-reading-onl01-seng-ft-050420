1522bee49ae57c695c26adb78f9a0dfdbb2808cc
Vianka Rivera <viankalrivera@gmail.com>
2020-05-22 02:46:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,6 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0].name
