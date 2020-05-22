5d83cbba450fe3b03d091ef8444a1f1ef3a38460
Vianka Rivera <viankalrivera@gmail.com>
2020-05-22 02:36:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN").text
-
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
